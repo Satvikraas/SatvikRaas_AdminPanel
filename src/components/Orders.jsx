0688fc394f6b4e6d5125cd82bdcef1e0c2495212
HaraayStudio <haraaydeployment@gmail.com>
2025-02-07 14:49:13 +0530

    added

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -268,7 +268,7 @@ function Orders() {
                   {orders.map((order, index) => (
                     <tr
                       key={index}
-                      className={order.status == "PAID" || order.status == "COD" || order.status == "FORWORDED TO LOGISTICS"? "greenbg" : ""}
+                      className={order.status == "PAID" || order.status == "COD" || order.status == "CREATED"? "greenbg" : ""}
                     >
                       <td>{order.razorpayOrderId}</td>
                       <td>{order.userName}</td>
